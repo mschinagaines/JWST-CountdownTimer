76bd4f35f03d3d7d704267bb9f1b047f52652e86
China G <61610421+mschinagaines@users.noreply.github.com>
2020-12-27 21:46:40 -0500

    🕑 Added countdown timer

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-const newBeginnings = '1 Jan 2021'
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('minutes');
+const secsEl = document.getElementById('seconds');
+
+const newBeginnings = '1 Jan 2021';
 
 function countdown() {
-    const newYearsDate = new Date(newYears);
+    const newDate = new Date(newBeginnings);
     const currentDate = new Date();
 
-    console.log(newYearsDate - currentDate);
+    const totalSeconds = (newDate - currentDate) / 1000;
+    
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minsEl.innerHTML = minutes;
+    secsEl.innerHTML = seconds;
 }
 
 // initial call
